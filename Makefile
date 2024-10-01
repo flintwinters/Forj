91d0b0c643e832bdfe9596c2dd3188680ffe30b5
flintwinters <flintwinters.0@gmail.com>
2024-10-01 12:45:40 -0400

    + rv64 asm utils

--- Makefile
+++ Makefile
@@ -11,6 +11,16 @@ rv:
 	gdb-multiarch -q -ex "source `pwd`/pyg.py";\
 	pkill qemu-riscv64
 
+asm:
+	cd rv64;\
+	riscv64-unknown-elf-as main.s -g -o outasm.o;\
+	riscv64-unknown-elf-ld outasm.o -o outasm;\
+	qemu-riscv64 -g 2222 outasm &\
+	# riscv64-unknown-elf-gcc -static -nostdlib -g -ggdb -O0 main.c -S main.s;\
+	gdb-multiarch -q -ex "source `pwd`/pyg.py";\
+	pkill qemu-riscv64;\
+	rm outasm.o
+
 obj:
 	cd rv64
 	riscv64-unknown-elf-objdump outmain
--- rv64/main.s
+++ rv64/main.s
@@ -4,16 +4,48 @@
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
 	.text
+	.globl	m
+	.bss
+	.align	3
+	.type	m, @object
+	.size	m, 40
+m:
+	.zero	40
+	.text
 	.align	1
 	.globl	_start
 	.type	_start, @function
 _start:
-	addi	sp,sp,-16
-	sd	s0,8(sp)
-	addi	s0,sp,16
-	nop
-	ld	s0,8(sp)
-	addi	sp,sp,16
-	jr	ra
+	addi	sp,sp,-32
+	sd	s0,24(sp)
+	addi	s0,sp,32
+.L4:
+	sw	zero,-20(s0)
+	j	.L2
+.L3:
+	lui	a5,%hi(m)
+	addi	a4,a5,%lo(m)
+	lw	a5,-20(s0)
+	slli	a5,a5,2
+	add	a5,a4,a5
+	lw	a5,0(a5)
+	lw	a4,-20(s0)
+	addw	a5,a4,a5
+	sext.w	a4,a5
+	lui	a5,%hi(m)
+	addi	a3,a5,%lo(m)
+	lw	a5,-20(s0)
+	slli	a5,a5,2
+	add	a5,a3,a5
+	sw	a4,0(a5)
+	lw	a5,-20(s0)
+	addiw	a5,a5,1
+	sw	a5,-20(s0)
+.L2:
+	lw	a5,-20(s0)
+	sext.w	a4,a5
+	li	a5,9
+	ble	a4,a5,.L3
+	j	.L4
 	.size	_start, .-_start
 	.ident	"GCC: (gc891d8dc23e) 13.2.0"
