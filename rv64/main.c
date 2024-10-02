dfadd074400ac8b3a67c53a73d6e6197f8547bb2
flintwinters <flintwinters.0@gmail.com>
2024-10-02 13:26:59 -0400

    - simplified main.c

--- rv64/main.c
+++ rv64/main.c
@@ -1,13 +1,4 @@
 // https://joe-degs.github.io/systems/2022/06/22/remote-debugging-gdb-qemu.html
-int m[10];
 void _start() {
-    while (1) {
-        for (int i = 0; i < 10; i++) {
-            m[i] += i;
-        }
-    }
+    while (1) {}
 }
-
-// int main() {
-//     printf("test");
-// }
