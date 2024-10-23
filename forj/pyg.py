30996d3a490d9dec888fd2a2478a7297471c9df5
flintwinters <flintwinters.0@gmail.com>
2024-10-22 21:30:29 -0400

    + debug aux files

---
+++ forj/pyg.py
@@ -0,0 +1,31 @@
+from toml import loads
+import gdb
+
+# https://stackoverflow.com/questions/2290842/gdb-python-scripting-where-has-parse-and-eval-gone
+def parseandeval(s):
+    gdb.execute("set logging redirect on")
+    gdb.execute("set logging enabled on")
+    gdb.execute("print " + s)
+    gdb.execute("set logging enabled off")
+    return gdb.history(0)
+
+def connect(): # arbitrary default
+    T = []
+    with open("debug.toml", "r") as f:
+        T = loads(f.read())
+    def buildcmds(s, l):
+        if l: return s + " " + ("\n"+s+" ").join(l)
+        return ""
+    breaks   = buildcmds("b ", T["breakpoints"])
+    displays = buildcmds("display ", T["displays"])
+    gdb.execute(f"""
+    exec-file ./forjlang
+    b main
+    run
+    d
+    {breaks}
+    run
+    {displays}
+    """)
+    
+connect()
