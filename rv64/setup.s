c41f727d3977574181e19be4db8f0497f24ef438
flintwinters <flintwinters.0@gmail.com>
2024-10-31 14:05:29 -0400

    interrupt experimentation

--- rv64/setup.s
+++ rv64/setup.s
@@ -29,9 +29,12 @@ _start:
 #
 # timer interrupt setup:
 #
+# 63 62-38 37  36 35-34 33-32 31-23 22  21 20  19  18 17   16-15 14-13 12-11 10-9  8   7    6    5   4    3   2    1    0
+# SD WPRI MBE SBE SXL   UXL   WPRI TSR TW TVM MXR SUM MPRV  XS   FS    MPP   VS   SPP MPIE UBE SPIE WPRI MIE WPRI SIE WPRI
+# 1   25   1   1   2    2      9    1  1   1   1   1   1    2    2     2     2     1   1   1    1    1    1    1   1    1
 	# enable global interrupts for m and s modes
 	# set previous privilege level to s
-    li      t0, 0x1882
+    li      t0, 0x1880
     csrw    mstatus, t0
 
 	# start mtimecmp at 40k
@@ -40,9 +43,9 @@ _start:
     sd      t0, 0(t1)
 
 
-	# # delegate m interrupts to s
-    # li      t0, 0x20
-    # csrs    mideleg, t0
+	# # delegate s interrupts to m
+    li      t0, 0x20
+    csrs    mideleg, t0
 
 	# set trap handler location
     la      t0, mtrap
@@ -65,6 +68,7 @@ _start:
     mret
 
 enterkernel:
+
 	j main
 
 mtrap:
@@ -72,17 +76,13 @@ mtrap:
 # general trap handler:
 #
 # all registers will need to be saved including tX
-	# unset interrupt pending
-	li      t0, 0x80
-    csrc    mip, t0
-
 	# parse mcause
 	# 3.1.15. Machine Cause Register (mcause)
-	csrr	t1, mcause
-	srli 	t0, t1, 0x3f
-	bnez 	t0, interrupts
+	csrr	t3, mcause
+	li 		t2, 0
+	blt 	t3, t2, interrupts
 
-	# non-interrupt codes:
+	# exception codes:
 	# 0 Instruction address misaligned
 	# 1 Instruction access fault
 	# 2 Illegal instruction
@@ -95,10 +95,10 @@ mtrap:
 	# 9 Environment call from S-mode
 	# 10 Reserved
 	# 11 Environment call from M-mode
-	# 12 Instruction page fault
-	# 13 Load page fault
+	# > 12 Instruction page fault
+	# > 13 Load page fault
 	# 14 Reserved
-	# 15 Store page fault
+	# > 15 Store page fault
 	# probably should use a switch statement for this
 	mret
 
@@ -118,47 +118,46 @@ storepage:			# 15
 	mret
 
 interrupts:
-	andi 	t1, t1, 0xf
+	andi 	t3, t3, 0xf
 
-	li		t0, 0x07
-	beq 	t0, t1, timerhandler
+	li		t2, 0x07
+	beq 	t2, t3, timerhandler
 	mret
 
 timerhandler:
 	# add 30k to mtimecmp
-	li      t1, 0x2004000
-	ld      t0, 0(t1)
-	li		t2, 30000
-	add 	t0, t0, t2
-	sd		t0, 0(t1)
+	li      t3, 0x2004000
+	ld      t2, 0(t3)
+	li		t4, 30000
+	add 	t2, t2, t4
+	sd		t2, 0(t3)
 
     mret
 
 # uart
 # https://github.com/safinsingh/ns16550a/blob/master/src/ns16550a.s
 serial:
-	# interrupts = <0x0a>;
-	# interrupt-parent = <0x03>;
-	# clock-frequency = "\08@";
-	# reg = <0x00 0x10000000 0x00 0x100>;
-	# compatible = "ns16550a";
 
 # kernel
 main:
+	la t1, _page_table_start
+	li t0, 0x8000
+	add t1, t1, t0
+	lb t0, 0(t1)
 	# Uart getchar:
-	li  t0, 0x10000000
+# 	li  t0, 0x10000000
 
-uartloop:
-	lb  	t1, 5(t0)
-	andi 	t1, t1, 0x1
-	beqz 	t1, nouart
+# uartloop:
+# 	lb  	t1, 5(t0)
+# 	andi 	t1, t1, 0x1
+# 	beqz 	t1, nouart
 	
-	lb 		t1, 0(t0)
-	sb 		t1, 0(t0)
+# 	lb 		t1, 0(t0)
+# 	sb 		t1, 0(t0)
 
-	j uartloop
+# 	j uartloop
 
-nouart:
+# nouart:
 	j main
 
 
